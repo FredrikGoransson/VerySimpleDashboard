3e784959b9a60f922b2a9e66b97d97b75c6b8e68
Fredrik Göransson <fredrik.goransson@gmail.com>
2014-09-24 09:47:58 +0200

    Created first VS project

---
+++ src/VerySimpleDashboard.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace VSD.Console
+{
+
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
